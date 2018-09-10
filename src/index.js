b163a5c3e3b5f87a16859c462abd6ea7cd88895c
udayanshevade <udayan.shevade@gmail.com>
2018-09-10 07:38:25 -0700

    add vertices and buffers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 let gl;
 let shaderProgram;
+let vertices;
 
 const initGL = () => {
   const canvas = document.getElementById('canvas');
@@ -40,17 +41,31 @@ const createShaders = () => {
 };
 
 const createVertices = () => {
+  vertices = [
+    -0.9, -0.9, 0,
+    0.9, -0.9, 0,
+    0, 0.9, 0
+  ];
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
   const coords = gl.getAttribLocation(shaderProgram, 'coords');
-  gl.vertexAttrib3f(coords, 0, 0, 0);
+  gl.vertexAttribPointer(coords, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(coords);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
   const pointSize = gl.getAttribLocation(shaderProgram, 'pointSize');
   gl.vertexAttrib1f(pointSize, 10);
+
   const color = gl.getUniformLocation(shaderProgram, 'color');
   gl.uniform4f(color, 1, 0, 1, 1);
 };
 
 const draw = () => {
   gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.POINTS, 0, 1);
+  gl.drawArrays(gl.LINE_LOOP, 0, 3);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
 };
 
 initGL();
