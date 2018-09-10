de2bca803730ffaa203982d67f0ed0f1d7e3cc99
udayanshevade <udayan.shevade@gmail.com>
2018-09-10 08:42:38 -0700

    add numerous vertices with varying point size

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 let gl;
 let shaderProgram;
 let vertices;
+const vertexCount = 5000;
+let buffer;
+let pointsGrowing = false;
+const POINT_DIMENSION_UPPER = 5;
+const POINT_DIMENSION_LOWER = 0;
+let pointDimension = 2;
 
 const initGL = () => {
   const canvas = document.getElementById('canvas');
@@ -41,31 +47,55 @@ const createShaders = () => {
 };
 
 const createVertices = () => {
-  vertices = [
-    -0.9, -0.9, 0,
-    0.9, -0.9, 0,
-    0, 0.9, 0
-  ];
-  const buffer = gl.createBuffer();
+  vertices = [];
+
+  for (let i = 0; i < vertexCount; i += 1) {
+    vertices.push(Math.random() * 2 - 1);
+    vertices.push(Math.random() * 2 - 1);
+  }
+
+  buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
   const coords = gl.getAttribLocation(shaderProgram, 'coords');
-  gl.vertexAttribPointer(coords, 3, gl.FLOAT, false, 0, 0);
+  gl.vertexAttribPointer(coords, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(coords);
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-  const pointSize = gl.getAttribLocation(shaderProgram, 'pointSize');
-  gl.vertexAttrib1f(pointSize, 10);
+  pointSize = gl.getAttribLocation(shaderProgram, 'pointSize');
+  gl.vertexAttrib1f(pointSize, pointDimension);
 
   const color = gl.getUniformLocation(shaderProgram, 'color');
-  gl.uniform4f(color, 1, 0, 1, 1);
+  gl.uniform4f(color, 1, 0, 0, 1);
 };
 
 const draw = () => {
+  for (let i = 0; i < vertexCount * 2; i += 2) {
+    vertices[i] += Math.random() * 0.01 - 0.005;
+    vertices[i + 1] += Math.random() * 0.01 - 0.005;
+  }
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+  if (pointDimension <= POINT_DIMENSION_LOWER) {
+    pointsGrowing = true;
+  } else if (pointDimension >= POINT_DIMENSION_UPPER) {
+    pointsGrowing = false;
+  }
+
+  if (pointsGrowing) {
+    pointDimension += Math.random() * 0.01;
+  } else {
+    pointDimension -= Math.random() * 0.01;
+  }
+
+  gl.vertexAttrib1f(pointSize, pointDimension);
+
   gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.LINE_LOOP, 0, 3);
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gl.drawArrays(gl.POINTS, 0, vertexCount);
+
+  requestAnimationFrame(draw);
 };
 
 initGL();
